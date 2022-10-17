1ecd37dc52193434f9ce259199e7fafabf393d9c
vantwill <vtwill41@gmail.com>
2022-10-16 19:25:16 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 exec { 'echo -e "IdentityFile ~/.ssh/school\n" >> /etc/ssh/ssh_config':
   provider => shell,
   path    => '/etc/ssh/ssh_config',
-  command => 'echo -e "PasswordAuthentication no\n" >> /etc/ssh/ssh_config'
+  command => 'echo -e "PasswordAuthentication off\n" >> /etc/ssh/ssh_config'
 }
